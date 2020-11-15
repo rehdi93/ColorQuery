94661ecfa16e1b2b75ccdc641a6f07c806334a57
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2020-11-15 18:03:31 -0300

    update aboutbox
    remove accessors

--- ColorQuery/AboutBox.xaml.cs
+++ ColorQuery/AboutBox.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Diagnostics;
 using System.Reflection;
 using System.Windows;
@@ -15,13 +16,19 @@ namespace ColorQuery
         public AboutBox()
         {
             InitializeComponent();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var attributes = assembly.GetCustomAttributes();
+            var aname = assembly.GetName();
+
+            Title = translate("About") + " " + aname.Name;
+            txtProductName.Text = attributes.OfType<AssemblyProductAttribute>().FirstOrDefault()?.Product;
+            txtVersion.Text = aname.Version.ToString();
+            txtCopyright.Text = attributes.OfType<AssemblyCopyrightAttribute>().FirstOrDefault()?.Copyright;
+
             btnOk.Click += delegate { Close(); };
             btnOk.Content = ApplicationCommands.Close.Text;
 
-            Title = translate("About") + ' ' + this.AssemblyTitle;
-            txtProductName.Text = this.AssemblyProduct;
-            txtVersion.Text = translate("Version") + ' ' + this.AssemblyVersion;
-            txtCopyright.Text = this.AssemblyCopyright;
             weblink.RequestNavigate += (_, e) => {
                 var psi = new ProcessStartInfo(e.Uri.ToString()) {
                     UseShellExecute = true
@@ -30,72 +37,5 @@ namespace ColorQuery
                 e.Handled = true;
             };
         }
-
-        #region Assembly Attribute Accessors
-        public string AssemblyTitle
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-                if (attributes.Length > 0)
-                {
-                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                    if (titleAttribute.Title != "")
-                    {
-                        return titleAttribute.Title;
-                    }
-                }
-                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
-            }
-        }
-
-        public string AssemblyVersion
-        {
-            get
-            {
-                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            }
-        }
-
-        public string AssemblyProduct
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyProductAttribute)attributes[0]).Product;
-            }
-        }
-
-        public string AssemblyCopyright
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-            }
-        }
-
-        public string AssemblyCompany
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCompanyAttribute)attributes[0]).Company;
-            }
-        }
-        #endregion
-
     }
 }
